317d1ef963d93489bd0af7c8a16b402234c6fc23
postmart <postmart@me.com>
2015-06-20 00:17:41 +0800

    changed name for app

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
 DELEGATE_IP=$(cat delegate_ip)
 echo -e "\e[32mpreparing test database"
-echo -e "\e[33mrunning from merlin"
+echo -e "\e[33mrunning from app"
 echo -e "\e[32mCreate database: testdb1"
 echo -e "\e[0m"
-docker exec merlin bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -c 'CREATE DATABASE testdb1;'"
+docker exec app bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -c 'CREATE DATABASE testdb1;'"
 sleep 3
 
-echo -e "\e[33mcreating table testtable1 from merlin"
+echo -e "\e[33mcreating table testtable1 from app"
 echo -e "\e[0m"
-docker exec merlin bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'CREATE TABLE testtable1 (i int);'"
+docker exec app bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'CREATE TABLE testtable1 (i int);'"
 sleep 3
 
-echo -e "\e[32minserting into testtable1 (i int) from merlin"
+echo -e "\e[32minserting into testtable1 (i int) from app"
 echo -e "\e[0m"
-docker exec merlin bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'INSERT INTO testtable1 values (0);'"
+docker exec app bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'INSERT INTO testtable1 values (0);'"
 sleep 2
 
 echo -e "\e[32m SELECT * from testtable1"
 echo -e "\e[0m"
-docker exec merlin bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'SELECT * from testtable1;'"
+docker exec app bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'SELECT * from testtable1;'"
 sleep 1
 
 echo -e "\e[32checking replication"
@@ -62,14 +62,14 @@ echo ".............................................."
 docker exec master /etc/init.d/postgresql stop
 sleep 3
 
-echo -e "\e[33m Inserting values into testable1 from merlin server: INSERT INTO testtable1 values (1)"
+echo -e "\e[33m Inserting values into testable1 from app server: INSERT INTO testtable1 values (1)"
 echo -e "\e[0m"
 sleep 1
-docker exec merlin bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'INSERT INTO testtable1 values (1);'"
+docker exec app bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'INSERT INTO testtable1 values (1);'"
 sleep 1
 echo "..............................................."
 echo -e "\e[33mSELECT * from testtable1"
 echo -e "\e[0m"
 sleep 1
-docker exec merlin bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'SELECT * from testtable1;'"
+docker exec app bash -c "sudo -u postgres psql -h $DELEGATE_IP -p 9999 -d testdb1 -c 'SELECT * from testtable1;'"
 
